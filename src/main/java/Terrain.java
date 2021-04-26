ab76d4a9b386adcf7039e3ce2ecfe3e2d3825a5e
LenaGuilland <guilland.lena@gmail.com>
2021-04-26 14:44:41 +0200

    etat initial

---
+++ src/main/java/Terrain.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author liiina55
+ */
+public class Terrain {
+    private double xmin;
+    private double xmax;
+    private double ymin;
+    private double ymax;
+    
+}
