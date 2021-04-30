8f2079ef68649a2c05585618fa5689392e916d2d
LenaGuilland <83221670+LenaGuilland@users.noreply.github.com>
2021-04-30 10:20:58 +0200

    Add files via upload

--- src/main/java/TypeBarre.java
+++ src/main/java/TypeBarre.java
@@ -13,6 +13,25 @@ public class TypeBarre {
     private double cout;
     private double Lmax;
     private double Lmin;
-    private double Rtension;
-    private double Rcompression;
+    private double Rtmax;
+    private double Rcmax;
+    private int ident;
+    
+    
+    
+    public static void bois(double cout, double Lmax, double Lmin,double Rtmax, double Rcmax){
+        cout = 4;
+        Lmax = 10;
+        Lmin = 5;
+        Rtmax = 6;
+        Rcmax = 3;
+    }
+    
+    public static void acier(double cout, double Lmax, double Lmin,double Rtmax, double Rcmax){
+        cout = 8;
+        Lmax = 20;
+        Lmin = 10;
+        Rtmax = 7;
+        Rcmax = 5;
+    }
 }
