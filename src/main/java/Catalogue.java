301a8f7681e4c4d4b564d0b3b71274ea6db64f2e
LenaGuilland <83221670+LenaGuilland@users.noreply.github.com>
2021-04-30 10:12:14 +0200

    Add files via upload

---
+++ src/main/java/Catalogue.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author liiina55
+ */
+public class Catalogue extends TypeBarre {
+    
+    public double[][]Cataogue(){
+        double[][] Catalogue= new double[2][5];
+        Catalogue[0][0]= ;
+        
+
+}
