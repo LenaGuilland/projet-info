4cd36703298250f1ff345ff2cbaee2bc28a82041
LenaGuilland <83221670+LenaGuilland@users.noreply.github.com>
2021-05-02 10:42:43 +0200

    Add files via upload

--- src/main/java/Catalogue.java
+++ src/main/java/Catalogue.java
@@ -8,11 +8,47 @@
  *
  * @author liiina55
  */
-public class Catalogue extends TypeBarre {
+public class Catalogue {
     
-    public double[][]Cataogue(){
-        double[][] Catalogue= new double[2][5];
-        Catalogue[0][0]= ;
+    private TypeBarre acier;
+    private TypeBarre bois;
+    
+    public Catalogue(){
+        this.acier = barreAcier();
+        this.bois = barreBois();
+    }
         
-
+    public TypeBarre barreAcier(){
+        acier.setCout(20);
+        acier.setLmax(10);
+        acier.setLmin(5);
+        acier.setRcmax(12);
+        acier.setRtmax(8);
+        return acier;
+    }
+    
+    public TypeBarre barreBois(){
+        bois.setCout(15);
+        bois.setLmax(9);
+        bois.setLmin(4);
+        bois.setRcmax(10);
+        bois.setRtmax(7);
+        return bois;
+    }
+    
+    public String toString(TypeBarre b){
+        String res="";
+        if(b==barreBois()){
+            res="cout : "+bois.getCout()+"€\n"+"longueur min : "+bois.getLmin()+"m\n"+"longueur max : "+bois.getLmax()+"m\n"+"résistance compression max : "+bois.getRcmax()+"\n"+"résistance tension max : "+bois.getRtmax();
+        }
+        else if(b==barreAcier()){
+            res="cout : "+acier.getCout()+"€\n"+"longueur min : "+acier.getLmin()+"m\n"+"longueur max : "+acier.getLmax()+"m\n"+"résistance compression max : "+acier.getRcmax()+"\n"+"résistance tension max : "+acier.getRtmax();
+        }
+        return res;
+    }
+    
+    public static void main(String[] args){
+        TypeBarre b= new TypeBarre();
+        System.out.println(b.toString());
+    }
 }
